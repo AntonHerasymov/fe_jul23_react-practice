83418c197ab4a6b4ba00129ce0c52920679c37e7
Anton Herasymov <antonherasymov2002@gmail.com>
2023-08-31 11:42:35 +0300

    created components

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,31 @@
+import cn from 'classnames';
+
+export const Product = ({ product }) => {
+  const { id, name, category } = product;
+  const { title, icon } = category;
+
+  return (
+    <tr data-cy="Product">
+      <td className="has-text-weight-bold" data-cy="ProductId">
+        {id}
+      </td>
+
+      <td data-cy="ProductName">{name}</td>
+      <td data-cy="ProductCategory">
+        <span role="img" aria-labelledby="emoji-desc">{icon}</span>
+        -
+        {title}
+      </td>
+
+      <td
+        data-cy="ProductUser"
+        className={cn({
+          'has-text-link': product.user.sex === 'm',
+          'has-text-danger': product.user.sex === 'f',
+        })}
+      >
+        {product.user.name}
+      </td>
+    </tr>
+  );
+};
---
+++ src/components/ProductTable/ProductTable.jsx
@@ -0,0 +1,76 @@
+import { ProductList } from '../ProductList/ProductList';
+
+export const ProductTable = ({ products }) => {
+  const x = '';
+
+  return (
+    <div className="box table-container">
+      {products.length === 0 && (
+        <p data-cy="NoMatchingMessage">
+          {`No products matching selected criteria ${x}`}
+        </p>
+      )}
+
+      <table
+        data-cy="ProductTable"
+        className="table is-striped is-narrow is-fullwidth"
+      >
+        <thead>
+          <tr>
+            <th>
+              <span className="is-flex is-flex-wrap-nowrap">
+                ID
+
+                <a href="#/">
+                  <span className="icon">
+                    <i data-cy="SortIcon" className="fas fa-sort" />
+                  </span>
+                </a>
+              </span>
+            </th>
+
+            <th>
+              <span className="is-flex is-flex-wrap-nowrap">
+                Product
+
+                <a href="#/">
+                  <span className="icon">
+                    <i data-cy="SortIcon" className="fas fa-sort-down" />
+                  </span>
+                </a>
+              </span>
+            </th>
+
+            <th>
+              <span className="is-flex is-flex-wrap-nowrap">
+                Category
+
+                <a href="#/">
+                  <span className="icon">
+                    <i data-cy="SortIcon" className="fas fa-sort-up" />
+                  </span>
+                </a>
+              </span>
+            </th>
+
+            <th>
+              <span className="is-flex is-flex-wrap-nowrap">
+                User
+
+                <a href="#/">
+                  <span className="icon">
+                    <i data-cy="SortIcon" className="fas fa-sort" />
+                  </span>
+                </a>
+              </span>
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <ProductList products={products} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
